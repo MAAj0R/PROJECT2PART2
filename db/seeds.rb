55891859552ee6e9e8eccb39b821e0e36e1844a1
Javier <maajorsinho@hotmail.com>
2017-10-10 01:35:48 -0300

    2DO COMMIT CAMBIOS SEED

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
-
 Language.create(name: 'English', image: 'http://www.metacinefilms.com/Img/flag/us.png')
 Language.create(name: 'Spanish', image: 'http://www.grupomastergrafic.net/WebRoot/StoreES3/Shops/64667427/5655/A1B0/1FD7/1459/0B72/C0A8/2BBA/CBD4/bandera-espana-con-escudo-para-exterior_s.jpg')
 Language.create(name: 'Portuguese', image: 'http://www.palimpalem.com/5/sudamerica/thumbs/thumb_bandera-brasil-8.jpg')
+
